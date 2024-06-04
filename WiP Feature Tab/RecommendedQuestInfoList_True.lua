f4f2fedcf8af4588f205754cf3251e5ee1fc06fb
Logy <130806188+ikatsuro@users.noreply.github.com>
2024-06-03 21:20:41 -0300

    Feature tab for Episode quests

---
+++ WiP Feature Tab/RecommendedQuestInfoList_True.lua
@@ -0,0 +1,294 @@
+RecommendedQuestInfoList = {}
+RecommendedQuestInfoList = {
+	[1] = {
+		Title = "Episode 12 Nightmare of Midgard",
+		IconName = "ico_ep.bmp",
+		Summary = "The Destruction of Morroc",
+		NpcSpr = "4_M_MOCASS1",
+		NpcNavi = "pontera",
+		NpcPosX = 164,
+		NpcPosY = 304,
+		QuestInfo1 = {
+			"Ages ago a great evil was conquered and sealed beneath the Desert town of Morroc. The great stirring of evil in the world has weakened those seals.",
+			"In a great upheaval which shook the entire world the evil awoke, in mere moments the quaint desert town of Morroc was razed, the citizens fleeing towards the Morroc Pyramids.",
+			"The deserts surrounding Morroc are changed forever, as the Extra-dimensional evil Satan Morocc roams free.",
+			"In response the armies of the world gather to fight before more of the world is plunged into chaos.",
+			"Chief Balrog is the senior leader of the Continental Guards, this volunteer army is the only thing holding Satan Morocc back from spreading his influence and destructive power over the world.",
+			"You must prove your worth to join the Continental Guards, to sign up and do your part to save Rune Midgard visit Chief Balrog near the South Morroc Kafra."
+		},
+		QuestInfo2 = {
+			"Required:",
+			"^990000Level 80 or higher^000000"
+		},
+		QuestInfo3 = {
+			"^ff0000You can check out the iROwiki guide <URL>[Here]<INFO>https://irowiki.org/wiki/Continental_Guard_Quest_(Resurrection_of_Satan_Morroc)</INFO></URL> ^000000",
+			"<URL>Description Source<INFO>https://web.archive.org/web/20120515063037/http://www.playragnarok.com/news/updatedetail.aspx?id=81&p=3</INFO></URL>"
+		}
+	},
+	[2] = {
+		Title = "Episode 13.1 Ash Vacuum",
+		IconName = "ico_ep.bmp",
+		Summary = "Onward to the New World",
+		NpcSpr = "4_M_MANAGER",
+		NpcNavi = "prt_cas",
+		NpcPosX = 207,
+		NpcPosY = 165,
+		QuestInfo1 = {
+			"In Rune Midgards recent history catastrophe and destruction came to pass. Satan Morocc was freed and annihilated Morroc and altered the world forever. But even during this darkest moment the light of Heroism shown through and hope still remains.",
+			"The invincible Morocc fled before his life was snuffed out by the Heroes of the world, the inter-dimensional Rift that his passage created is a landmark to heroism`s resolve in these legendary days.",
+			" But even disaster brings opportunity. It seems only fitting that if Morocc invades our land, it is only fair that we invade his! So now eager adventurers are planning extended campaigns into what is being called the 'Ash Vacuum'.",
+			"This strange land that is so different from Rune Midgard still can be won by the spirit of Adventure and Bravery!"
+		},
+		QuestInfo2 = {
+			"Required:",
+			"^990000Level 70 or higher^000000"
+		},
+		QuestInfo3 = {
+			"^ff0000You can check out the iROwiki guide <URL>[Here]<INFO>https://irowiki.org/wiki/Onward_to_the_New_World_Quest</INFO></URL> ^000000",
+			"<URL>Description Source<INFO>https://web.archive.org/web/20120515063404/http://www.playragnarok.com/news/updatedetail.aspx?id=73&p=3</INFO></URL>"
+		}
+	},
+	[3] = {
+		Title = "Episode 13.2 Into the Unknown",
+		IconName = "ico_ep.bmp",
+		Summary = "Two Tribes",
+		NpcSpr = "4_M_FAIRYSOLDIER",
+		NpcNavi = "spl_in01",
+		NpcPosX = 32,
+		NpcPosY = 306,
+		QuestInfo1 = {
+			"The discovery of the Ash Vacuum has brought new reasons for adventurers to brave the unknown. Two new races have been discovered known only as the <STRONG>Laphine</STRONG> and the Sapha.",
+			"The Laphine are a fairy-like race that live in the lush grounds west of the Expedition camp.",
+			"The Sapha are a proud tribe of wood creatures that live in the barren snow grounds east of the Expedition camp.",
+			"Each race has sworn to protect the World Tree Yggdrasil and have been fighting a war spanning hundreds of years.",
+			"The Rune Midgard scientists have discovered ruins in a cave west of the Expedition camp and have reason to believe that it leads to the roots of the World Tree Yggdrasil.",
+			"Commander Hibba Agip, leader of the Expedition, has commanded an official investigation of these ruins."
+		},
+		QuestInfo2 = {
+			"Required:",
+			"^990000Level 70 or higher^000000",
+			"<ITEM>Ring of the Ancient Wise King<INFO>2782</INFO></ITEM>"
+		},
+		QuestInfo3 = {
+			"^ff0000You can check out the iROwiki guide <URL>[Here]<INFO>https://irowiki.org/wiki/Two_Tribes</INFO></URL> ^000000",
+			"<URL>Description Source<INFO>https://web.archive.org/web/20120515061454/http://www.playragnarok.com/news/updatedetail.aspx?id=68&p=2</INFO></URL>"
+		}
+	},
+	[4] = {
+		Title = "Episode 13.3 El Dicastes",
+		IconName = "ico_ep.bmp",
+		Summary = "El Dicastes, a city built on ore",
+		NpcSpr = "4_MAN_JERUTOO",
+		NpcNavi = "mid_camp",
+		NpcPosX = 222,
+		NpcPosY = 246,
+		QuestInfo1 = {
+			"Invitation from the Sapha tribe The Sapha tribe sent a messenger to Midgard Expedition Camp and the whole post is excited about it.",
+			"Those seeking to travel to the capital city of the Sapha people can begin their adventure at the Rune Midgard Allied Forces Post in the New World."
+		},
+		QuestInfo2 = {
+			"Required:",
+			"^990000Level 70 or higher^000000",
+			"<ITEM>Ring of the Ancient Wise King<INFO>2782</INFO></ITEM>"
+		},
+		QuestInfo3 = {
+			"^ff0000You can check out the iROwiki guide <URL>[Here]<INFO>https://irowiki.org/wiki/Sapha%27s_Visit</INFO></URL> ^000000",
+			"<URL>Description Source<INFO>https://web.archive.org/web/20110913131848/http://www.playragnarok.com/news/updatedetail.aspx?id=178&p=1</INFO></URL>"
+		}
+	},
+	[5] = {
+		Title = "Episode 14.1 Bifrost",
+		IconName = "ico_ep.bmp",
+		Summary = "The Wandering Guardian",
+		NpcSpr = "4_F_NYDHOG",
+		NpcNavi = "bif_fild01",
+		NpcPosX = 158,
+		NpcPosY = 340,
+		QuestInfo1 = {
+			"The guardian of the Bifrost, bridge between Midgard and Asgard, began losing it's soul to lyrical-like temptations.",
+			"As thousands of years passed, she wondered why she used all of her strength to guard the Bifrost. She became confused and chose to seal herself up rather than live in fear.",
+			"When she could no longer resist the lyrical temptation a massive crack broke through the bridge connecting Jotunheim and Alfheim, causing the the two worlds to be severed.",
+			"The passage through was now nothing but a mysterious fog.",
+			"With the origin unknown, no one dared to pass through.",
+			"Some adventurers have chosen to enter the fog but none have returned. Their fates have likely fallen into the same path as the nameless guardian maiden who locked away her confusion and despair into the dense fog."
+		},
+		QuestInfo2 = {
+			"Required:",
+			"^990000Level 99 or higher^000000",
+		},
+		QuestInfo3 = {
+			"^ff0000You can check out the iROwiki guide <URL>[Here]<INFO>https://irowiki.org/wiki/Hazy_Forest</INFO></URL> ^000000",
+			"<URL>Description Source<INFO>https://web.archive.org/web/20120510024749/http://www.playragnarok.com/news/updatedetail.aspx?id=185&p=1</INFO></URL>"
+		}
+	},
+	[6] = {
+		Title = "Episode 14.2 Eclage",
+		IconName = "ico_ep.bmp",
+		Summary = "Alfheim the land of the Laphine Fairies",
+		NpcSpr = "4_M_FARIYKING",
+		NpcNavi = "ecl_fild01",
+		NpcPosX = 100,
+		NpcPosY = 323,
+		QuestInfo1 = {
+			"In their efforts to continue to trace the path of Satan Morroc, brave adventurers crossed the legendary Bifrost bridge and discovered a lost city, Eclage. There they found that the Laphines of Eclage were completely separated from their brothers in Splendide because of the Hazy Forest Labyrinth.",
+			"The city of Eclage was built around a giant tree and is the main base of the Laphines who created it to combat the Giants of Jotunheim. Be one of the first adventurers to explore this new land and help uncover the mysteries it has to offer!"
+		},
+		QuestInfo2 = {
+			"Required:",
+			"No level restriction for Access",
+			"^990000Level 99 or higher^000000 for main quest",
+			"^990000Level 120 or higher^000000 for side quests"
+		},
+		QuestInfo3 = {
+			"^ff0000You can check out the iROwiki guide <URL>[Here]<INFO>https://irowiki.org/wiki/Eclage</INFO></URL> ^000000",
+			"<URL>Description Source<INFO>https://renewal.playragnarok.com/news/updatedetail.aspx?id=230&p=7</INFO></URL>"
+		}
+	},
+	[7] = {
+		Title = "Episode 14.3 Battle for Midgard",
+		IconName = "ico_ep.bmp",
+		Summary = "Dimensional Travel",
+		NpcSpr = "4_F_EDEN_OFFICER",
+		NpcNavi = "moc_para01",
+		NpcPosX = 38,
+		NpcPosY = 175,
+		QuestInfo1 = {
+			"The Flame Basin: A pocket dimension on the other side of the Dimensional Gap where Satan Morroc took up residence after escaping from Rune Midgard.",
+			"Obsessed with creating a world of his own to dominate, Satan Morroc laid himself down on the new land to corrupt it with his own essence.",
+			"His blood pooled into waterfalls and rivers, his bones piled into dark rocky mountains. His scales crumbled to form bleak forests.",
+			"The Flame basin used to be a dead land where nothing could thrive, but Satan Morroc turned it into a breeding ground for vile demon creatures."
+		},
+		QuestInfo2 = {
+			"Required:",
+			"^990000Level 140 or higher^000000",
+			"Access to the New World"
+		},
+		QuestInfo3 = {
+			"^ff0000You can check out the iROwiki guide <URL>[Here]<INFO>https://irowiki.org/wiki/Dimensional_Travel</INFO></URL> ^000000",
+			"<URL>Description Source<INFO>https://renewal.playragnarok.com/news/updatedetail.aspx?id=271&p=5</INFO></URL>"
+		}
+	},
+	[8] = {
+		Title = "Episode 15 Fantasmagorica",
+		IconName = "ico_ep.bmp",
+		Summary = "Scientific civilization beyond Juperos",
+		NpcSpr = "4_F_EDEN_OFFICER",
+		NpcNavi = "moc_para01",
+		NpcPosX = 38,
+		NpcPosY = 175,
+		BgName = "reco_99.bmp",
+		QuestInfo1 = {
+			"We are recruiting excavators to participate in the newly discovered Fantasmagorica study.",
+			"Wouldn't you like to explore scientific civilization that goes beyond Juperos?",
+			"If you want to join the excavation team, come to the Eden Group right now!"
+		},
+		QuestInfo2 = {
+			"Required:",
+			"^990000Level 140 or higher^000000",
+			"A person who joined the Eden Group and holds the Eden Group Mark"
+		},
+		QuestInfo3 = {
+			"^ff0000You can check out the iROwiki guide <URL>[Here]<INFO>https://irowiki.org/wiki/To_Phantasmagorika!</INFO></URL> ^000000"
+		}
+	},
+	[9] = {
+		Title = "Episode 16.1 Banquet of Heroes",
+		IconName = "ico_ep.bmp",
+		Summary = "You who worked hard! Enjoy it!!!",
+		NpcSpr = "4_M_ZONDAOYAJI",
+		NpcNavi = "prontera",
+		NpcPosX = 121,
+		NpcPosY = 72,
+		BgName = "reco_98.bmp",
+		QuestInfo1 = {
+			"We are holding a banquet to celebrate the subjugation of Morroc with heroes who participated in the expedition.",
+			"If you are an adventurer who participated in the expedition, please accept the banquet invitation!"
+		},
+		QuestInfo2 = { "Required:", "Access to the New World", "^990000Level 100 or higher^000000" },
+		QuestInfo3 = {
+			"^ff0000You can check out the iROwiki guide <URL>[Here]<INFO>https://irowiki.org/wiki/The_Royal_Banquet</INFO></URL> ^000000"
+		}
+	},
+	[10] = {
+		Title = "Episode 16.2 Terra Gloria",
+		IconName = "ico_ep.bmp",
+		Summary = "A journey to regain the symbol of the missing royal family",
+		NpcSpr = "4_EP16_NIHIL",
+		NpcNavi = "prt_cas_q",
+		NpcPosX = 21,
+		NpcPosY = 39,
+		BgName = "reco_97.bmp",
+		QuestInfo1 = {
+			"The young blood of the royal family, Nihil and Spica, want you to join the secret operation.",
+			"Please join us on a journey to regain the symbol of the missing royal family!"
+		},
+		QuestInfo2 = { "Required:", "Episode ^99000016.1^000000 Complete", "^990000Level 100 or higher^000000" },
+		QuestInfo3 = {
+			"^ff0000You can check out the iROwiki guide <URL>[Here]<INFO>https://irowiki.org/wiki/Terra_Gloria</INFO></URL> ^000000"
+		}
+	},
+	[11] = {
+		Title = "Episode 17.1 Illusion",
+		IconName = "ico_ep.bmp",
+		Summary = "A full- scale operation begins.",
+		NpcSpr = "4_M_PHILOFONTES",
+		NpcNavi = "pub_cat",
+		NpcPosX = 93,
+		NpcPosY = 105,
+		BgName = "reco_96.bmp",
+		QuestInfo1 = {
+			"The stage of the Episode is moved to Schwaltzvalt!",
+			"A full-fledged joint operation begins with the Rebelion and the Secret Wings.",
+			"Rebellion, which also established an operational base in Einbroch, will target secret research facilities found with information brought by Secret Wings..."
+		},
+		QuestInfo2 = { "Required:", "Episode ^99000016.2^000000 Complete", "^990000Level 110 or higher^000000" },
+		QuestInfo3 = {
+			"^ff0000You can check out the iROwiki guide <URL>[Here]<INFO>https://irowiki.org/wiki/Illusion</INFO></URL> ^000000"
+		}
+	},
+	[12] = {
+		Title = "Episode 17.2 Sage's Legacy",
+		IconName = "ico_ep.bmp",
+		Summary = "I must not fall behind...",
+		NpcSpr = "4_M_ROOKIE",
+		NpcNavi = "sp_cor",
+		NpcPosX = 255,
+		NpcPosY = 285,
+		BgName = "reco_03.bmp",
+		QuestInfo1 = {
+			"The sewer pipe at a corner of Special Security Area, Cor has finally been opened.",
+			"The Rebellion and the members go into the sewer pipe in order to track Illusion."
+		},
+		QuestInfo2 = {
+			"Prerequisites",
+			"Episode ^99000017.1^000000 Complete",
+			"^990000Level 130 or higher^000000"
+		},
+		QuestInfo3 = {
+			"^ff0000You can check out the iROwiki guide <URL>[Here]<INFO>https://irowiki.org/wiki/Legacy_of_the_Wise_One</INFO></URL> ^000000"
+		}
+	},
+	[13] = {
+		Title = "Episode 18: Direction of Prayer",
+		IconName = "ico_ep.bmp",
+		Summary = "What's happening in Rachel?",
+		NpcSpr = "4_EP17_TABLET",
+		NpcNavi = "ba_in01",
+		NpcPosX = 26,
+		NpcPosY = 266,
+		BgName = "reco_80.bmp",
+		QuestInfo1 = {
+			"The slow-moving signal analysis of the airship has finally been completed!",
+			"It seems that the remnants of Illusion are plotting something in Rachel, the capital of Arunafeltz.",
+			"Eli decides to go to Rachel herself alongside the adventurer."
+		},
+		QuestInfo2 = {
+			"Prerequisites",
+			"Episode ^99000017.2^000000 Complete",
+			"^990000Level 170 or higher^000000"
+		},
+		QuestInfo3 = {
+			"^ff0000You can check out the iROwiki guide <URL>[Here]<INFO>https://irowiki.org/wiki/Direction_of_Prayer</INFO></URL> ^000000"
+		}
+	}
+}
